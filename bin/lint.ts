53730a2c437a2dd451d6c9fc945615197df3b605
Nick Amoscato <nick@amoscato.com>
2020-01-05 16:36:51 -0500

    Decrease parallel limit

--- bin/lint.ts
+++ bin/lint.ts
@@ -1,24 +1,28 @@
 import { readdirSync } from 'fs';
 import { join } from 'path';
 
+const MAX_PARALLEL = Number(process.env.GAPI_MAX_PARALLEL) || 1;
+
+process.stdout.setMaxListeners(MAX_PARALLEL);
+process.stderr.setMaxListeners(MAX_PARALLEL);
+
 const path = process.argv[2];
 const runAll = require('npm-run-all');
 
-console.log(`Reading project directories in ${path}`);
+console.log(`Reading project directories in ${path}...`);
 
 const scripts = readdirSync(path, { withFileTypes: true })
   .filter(dir => dir.isDirectory())
   .map(dir => `dtslint ${join(path, dir.name)}`);
 
 const options = {
-  maxParallel: scripts.length,
+  maxParallel: MAX_PARALLEL,
   parallel: true,
   stdout: process.stdout,
   stderr: process.stderr
 };
 
-process.stdout.setMaxListeners(scripts.length);
-process.stderr.setMaxListeners(scripts.length);
+console.log(`Linting ${scripts.length} projects in ${MAX_PARALLEL} parallel processes...`);
 
 runAll([scripts.shift()], options) // run first synchronously to install TypeScript
   .then(() => runAll(scripts, options))
