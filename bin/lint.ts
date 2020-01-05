c5079984f53c495598933885ddf262b9477df8ab
Nick Amoscato <nick@amoscato.com>
2020-01-05 15:48:32 -0500

    Lint first project synchronously

--- bin/lint.ts
+++ bin/lint.ts
@@ -3,7 +3,8 @@ import { join } from 'path';
 
 const MAX_PARALLEL = 20;
 
-process.setMaxListeners(MAX_PARALLEL);
+process.stdout.setMaxListeners(MAX_PARALLEL);
+process.stderr.setMaxListeners(MAX_PARALLEL);
 
 const path = process.argv[2];
 const runAll = require('npm-run-all');
@@ -14,11 +15,13 @@ const scripts = readdirSync(path, { withFileTypes: true })
   .filter(dir => dir.isDirectory())
   .map(dir => `dtslint ${join(path, dir.name)}`);
 
-runAll(scripts, {
+const options = {
   maxParallel: MAX_PARALLEL,
   parallel: true,
   stdout: process.stdout,
   stderr: process.stderr
-}).catch(err => {
-  process.exit(err.code)
-});
+};
+
+runAll([scripts.shift()], options) // run first synchronously to install TypeScript
+  .then(() => runAll(scripts, options))
+  .catch(err => process.exit(err.code));
