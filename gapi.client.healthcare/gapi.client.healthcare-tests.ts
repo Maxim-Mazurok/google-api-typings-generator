9c4ae8b315f79a337cf18389bda0e86e36f1de20
Maxim-Mazurok <Maxim-Mazurok@users.noreply.github.com>
2020-04-10 15:20:18 +0000

    Automated deployment: Fri Apr 10 15:20:18 UTC 2020 b3827a5d02afec697e5bd15317df1658f8d3c610

--- gapi.client.healthcare/gapi.client.healthcare-tests.ts
+++ gapi.client.healthcare/gapi.client.healthcare-tests.ts
@@ -5,7 +5,7 @@
 **/
 gapi.load('client', () => {
     /** now we can use gapi.client */
-    gapi.client.load('healthcare', 'v1beta1', () => {
+    gapi.client.load('healthcare', 'v1', () => {
         /** now we can use gapi.client.healthcare */
 
         /** don't forget to authenticate your client before sending any request to resources: */
