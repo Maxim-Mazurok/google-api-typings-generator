c01dc2258ef8e29e24fa452f50ada8381a2370ba
Maxim Mazurok <maxim@mazurok.com>
2020-04-13 20:20:10 +1000

    add sync before any command

--- bin/auto-update/sh.ts
+++ bin/auto-update/sh.ts
@@ -9,7 +9,7 @@ export class SH {
 
   runSh = async (command: string, cwd?: string): Promise<SpawnResult> => {
     console.log(command);
-    return spawnAsync(command, undefined, {
+    return spawnAsync(`sync && ${command}`, undefined, {
       shell: true,
       cwd: cwd || this.cwd,
     });
