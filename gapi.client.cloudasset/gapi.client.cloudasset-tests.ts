5ef610a42ca2b91793151a905cbf1c67c609857c
Maxim-Mazurok <Maxim-Mazurok@users.noreply.github.com>
2020-05-08 22:23:11 +0000

    Automated deployment: Fri May  8 22:23:10 UTC 2020 0f62b454ce67b6466e0e4c95dbb039b80f259e80

--- gapi.client.cloudasset/gapi.client.cloudasset-tests.ts
+++ gapi.client.cloudasset/gapi.client.cloudasset-tests.ts
@@ -108,10 +108,7 @@ gapi.load('client', () => {
          * Exports assets with time and resource types to a given Cloud Storage
          * location. The output format is newline-delimited JSON.
          * This API implements the google.longrunning.Operation API allowing you
-         * to keep track of the export. We recommend intervals of at least 2 seconds
-         * with exponential retry to poll the export operation result. For
-         * regular-size resource parent, the export operation usually finishes within
-         * 5 minutes.
+         * to keep track of the export.
          */
         await gapi.client.cloudasset.v1.exportAssets({
             parent: "Test string",
@@ -131,32 +128,6 @@ gapi.load('client', () => {
                 },
             },
             readTime: "Test string",
-        });
-        /**
-         * Searches all the IAM policies within the given accessible scope (e.g., a
-         * project, a folder or an organization). Callers should have
-         * cloud.assets.SearchAllIamPolicies permission upon the requested scope,
-         * otherwise the request will be rejected.
-         */
-        await gapi.client.cloudasset.v1.searchAllIamPolicies({
-            pageSize: 42,
-            pageToken: "Test string",
-            query: "Test string",
-            scope: "Test string",
-        });
-        /**
-         * Searches all the resources within the given accessible scope (e.g., a
-         * project, a folder or an organization). Callers should have
-         * cloud.assets.SearchAllResources permission upon the requested scope,
-         * otherwise the request will be rejected.
-         */
-        await gapi.client.cloudasset.v1.searchAllResources({
-            assetTypes: "Test string",
-            orderBy: "Test string",
-            pageSize: 42,
-            pageToken: "Test string",
-            query: "Test string",
-            scope: "Test string",
         });
     }
 });
