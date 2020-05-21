7dd28f37da8ca57bf258b93830c8fb3773d9265d
Maxim-Mazurok <Maxim-Mazurok@users.noreply.github.com>
2020-05-21 00:28:24 +0000

    Automated deployment: Thu May 21 00:28:24 UTC 2020 31580a71608474e7f5e9158d62432d8a9327b74e

--- gapi.client.firebaseml/gapi.client.firebaseml-tests.ts
+++ gapi.client.firebaseml/gapi.client.firebaseml-tests.ts
@@ -5,7 +5,7 @@
 **/
 gapi.load('client', () => {
     /** now we can use gapi.client */
-    gapi.client.load('firebaseml', 'v1beta2', () => {
+    gapi.client.load('firebaseml', 'v1', () => {
         /** now we can use gapi.client.firebaseml */
 
         /** don't forget to authenticate your client before sending any request to resources: */
@@ -27,5 +27,48 @@ gapi.load('client', () => {
     });
 
     async function run() {
+        /**
+         * Starts asynchronous cancellation on a long-running operation.  The server
+         * makes a best effort to cancel the operation, but success is not
+         * guaranteed.  If the server doesn't support this method, it returns
+         * `google.rpc.Code.UNIMPLEMENTED`.  Clients can use
+         * Operations.GetOperation or
+         * other methods to check whether the cancellation succeeded or whether the
+         * operation completed despite cancellation. On successful cancellation,
+         * the operation is not deleted; instead, it becomes an operation with
+         * an Operation.error value with a google.rpc.Status.code of 1,
+         * corresponding to `Code.CANCELLED`.
+         */
+        await gapi.client.firebaseml.operations.cancel({
+            name: "Test string",
+        }, {
+        });
+        /**
+         * Deletes a long-running operation. This method indicates that the client is
+         * no longer interested in the operation result. It does not cancel the
+         * operation. If the server doesn't support this method, it returns
+         * `google.rpc.Code.UNIMPLEMENTED`.
+         */
+        await gapi.client.firebaseml.operations.delete({
+            name: "Test string",
+        });
+        /**
+         * Lists operations that match the specified filter in the request. If the
+         * server doesn't support this method, it returns `UNIMPLEMENTED`.
+         *
+         * NOTE: the `name` binding allows API services to override the binding
+         * to use different resource name schemes, such as `users/&#42;/operations`. To
+         * override the binding, API services can add a binding such as
+         * `"/v1/{name=users/&#42;}/operations"` to their service configuration.
+         * For backwards compatibility, the default name includes the operations
+         * collection id, however overriding users must ensure the name binding
+         * is the parent resource, without the operations collection id.
+         */
+        await gapi.client.firebaseml.operations.list({
+            filter: "Test string",
+            name: "Test string",
+            pageSize: 42,
+            pageToken: "Test string",
+        });
     }
 });
--- gapi.client.networkmanagement/gapi.client.networkmanagement-tests.ts
+++ gapi.client.networkmanagement/gapi.client.networkmanagement-tests.ts
@@ -5,7 +5,7 @@
 **/
 gapi.load('client', () => {
     /** now we can use gapi.client */
-    gapi.client.load('networkmanagement', 'v1beta1', () => {
+    gapi.client.load('networkmanagement', 'v1', () => {
         /** now we can use gapi.client.networkmanagement */
 
         /** don't forget to authenticate your client before sending any request to resources: */
