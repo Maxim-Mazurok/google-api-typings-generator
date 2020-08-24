1343a2dadfaf96124ec24056fd89e4d9ef746d91
Maxim-Mazurok <Maxim-Mazurok@users.noreply.github.com>
2020-08-24 18:32:47 +0000

    Automated deployment: Mon Aug 24 18:32:46 UTC 2020 b5f4481d28c976c4076fd10d84ff072f8b1393c8

--- gapi.client.cloudbilling/gapi.client.cloudbilling-tests.ts
+++ gapi.client.cloudbilling/gapi.client.cloudbilling-tests.ts
@@ -12,6 +12,10 @@ gapi.load('client', () => {
         /** declare client_id registered in Google Developers Console */
         const client_id = '<<PUT YOUR CLIENT ID HERE>>';
         const scope = [
+            /** View and manage your Google Cloud Platform billing accounts */
+            'https://www.googleapis.com/auth/cloud-billing',
+            /** View your Google Cloud Platform billing accounts */
+            'https://www.googleapis.com/auth/cloud-billing.readonly',
             /** View and manage your data across Google Cloud Platform services */
             'https://www.googleapis.com/auth/cloud-platform',
         ];
