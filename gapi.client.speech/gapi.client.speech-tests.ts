d9c5f9587f672555d0330a9116c04c005bb7f288
Maxim-Mazurok <Maxim-Mazurok@users.noreply.github.com>
2020-08-27 17:26:32 +0000

    Automated deployment: Thu Aug 27 17:26:32 UTC 2020 2cd73d89673e27dfd5563756f8579a560dbd4d85

--- gapi.client.speech/gapi.client.speech-tests.ts
+++ gapi.client.speech/gapi.client.speech-tests.ts
@@ -27,25 +27,15 @@ gapi.load('client', () => {
     });
 
     async function run() {
-        /**
-         * Gets the latest state of a long-running operation.  Clients can use this
-         * method to poll the operation result at intervals as recommended by the API
-         * service.
-         */
+        /** Gets the latest state of a long-running operation. Clients can use this method to poll the operation result at intervals as recommended by the API service. */
         await gapi.client.speech.operations.get({
             name: "Test string",
         });
         /**
-         * Lists operations that match the specified filter in the request. If the
-         * server doesn't support this method, it returns `UNIMPLEMENTED`.
-         *
-         * NOTE: the `name` binding allows API services to override the binding
-         * to use different resource name schemes, such as `users/∗/operations`. To
-         * override the binding, API services can add a binding such as
-         * `"/v1/{name=users/*}/operations"` to their service configuration.
-         * For backwards compatibility, the default name includes the operations
-         * collection id, however overriding users must ensure the name binding
-         * is the parent resource, without the operations collection id.
+         * Lists operations that match the specified filter in the request. If the server doesn't support this method, it returns `UNIMPLEMENTED`. NOTE: the `name` binding allows API services to
+         * override the binding to use different resource name schemes, such as `users/∗/operations`. To override the binding, API services can add a binding such as
+         * `"/v1/{name=users/*}/operations"` to their service configuration. For backwards compatibility, the default name includes the operations collection id, however overriding users must
+         * ensure the name binding is the parent resource, without the operations collection id.
          */
         await gapi.client.speech.operations.list({
             filter: "Test string",
@@ -54,12 +44,8 @@ gapi.load('client', () => {
             pageToken: "Test string",
         });
         /**
-         * Performs asynchronous speech recognition: receive results via the
-         * google.longrunning.Operations interface. Returns either an
-         * `Operation.error` or an `Operation.response` which contains
-         * a `LongRunningRecognizeResponse` message.
-         * For more information on asynchronous speech recognition, see the
-         * [how-to](https://cloud.google.com/speech-to-text/docs/async-recognize).
+         * Performs asynchronous speech recognition: receive results via the google.longrunning.Operations interface. Returns either an `Operation.error` or an `Operation.response` which contains
+         * a `LongRunningRecognizeResponse` message. For more information on asynchronous speech recognition, see the [how-to](https://cloud.google.com/speech-to-text/docs/async-recognize).
          */
         await gapi.client.speech.speech.longrunningrecognize({
         }, {
@@ -102,10 +88,7 @@ gapi.load('client', () => {
                 useEnhanced: true,
             },
         });
-        /**
-         * Performs synchronous speech recognition: receive results after all audio
-         * has been sent and processed.
-         */
+        /** Performs synchronous speech recognition: receive results after all audio has been sent and processed. */
         await gapi.client.speech.speech.recognize({
         }, {
             audio: {
