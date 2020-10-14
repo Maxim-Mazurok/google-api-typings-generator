98a3079122e1d03bdce8c44ae2eeeff65180fa6d
Maxim-Mazurok <Maxim-Mazurok@users.noreply.github.com>
2020-10-14 05:50:23 +0000

    Automated deployment: Wed Oct 14 05:50:23 UTC 2020 498bc4456ce5b6c3a7da2abd4a25804e793cb61c

--- gapi.client.classroom/index.d.ts
+++ gapi.client.classroom/index.d.ts
@@ -241,11 +241,11 @@ declare namespace gapi.client {
             updateTime?: string;
         }
         interface Date {
-            /** Day of month. Must be from 1 to 31 and valid for the year and month, or 0 if specifying a year by itself or a year and month where the day is not significant. */
+            /** Day of a month. Must be from 1 to 31 and valid for the year and month, or 0 to specify a year by itself or a year and month where the day isn't significant. */
             day?: number;
-            /** Month of year. Must be from 1 to 12, or 0 if specifying a year without a month and day. */
+            /** Month of a year. Must be from 1 to 12, or 0 to specify a year without a month and day. */
             month?: number;
-            /** Year of date. Must be from 1 to 9999, or 0 if specifying a date without a year. */
+            /** Year of the date. Must be from 1 to 9999, or 0 to specify a date without a year. */
             year?: number;
         }
         interface DriveFile {
