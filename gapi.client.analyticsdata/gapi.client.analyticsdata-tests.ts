52dd38a45b28ca08ae2a9ad1221a5b352c451be6
Maxim-Mazurok <Maxim-Mazurok@users.noreply.github.com>
2020-10-14 16:59:40 +0000

    Automated deployment: Wed Oct 14 16:59:40 UTC 2020 498bc4456ce5b6c3a7da2abd4a25804e793cb61c

--- gapi.client.analyticsdata/gapi.client.analyticsdata-tests.ts
+++ gapi.client.analyticsdata/gapi.client.analyticsdata-tests.ts
@@ -4,7 +4,7 @@
 * In case of any problems please post issue to https://github.com/Maxim-Mazurok/google-api-typings-generator
 **/
 
-// Revision: 20201012
+// Revision: 20201013
 
 gapi.load('client', () => {
     /** now we can use gapi.client */
@@ -33,8 +33,8 @@ gapi.load('client', () => {
 
     async function run() {
         /**
-         * Returns metadata for dimensions and metrics available in reporting methods. Used to explore the dimensions and metrics. In this method, a Google Analytics App + Web Property Identifier
-         * is specified in the request, and the metadata response includes Custom dimensions and metrics as well as Universal metadata. For example if a custom metric with parameter name
+         * Returns metadata for dimensions and metrics available in reporting methods. Used to explore the dimensions and metrics. In this method, a Google Analytics GA4 Property Identifier is
+         * specified in the request, and the metadata response includes Custom dimensions and metrics as well as Universal metadata. For example if a custom metric with parameter name
          * `levels_unlocked` is registered to a property, the Metadata response will contain `customEvent:levels_unlocked`. Universal metadata are dimensions and metrics applicable to any property
          * such as `country` and `totalUsers`.
          */
--- gapi.client.analyticsdata/readme.md
+++ gapi.client.analyticsdata/readme.md
@@ -62,7 +62,7 @@ After that you can use Google Analytics Data API resources:
 ```typescript
 
 /*
-Returns metadata for dimensions and metrics available in reporting methods. Used to explore the dimensions and metrics. In this method, a Google Analytics App + Web Property Identifier is specified in the request, and the metadata response includes Custom dimensions and metrics as well as Universal metadata. For example if a custom metric with parameter name `levels_unlocked` is registered to a property, the Metadata response will contain `customEvent:levels_unlocked`. Universal metadata are dimensions and metrics applicable to any property such as `country` and `totalUsers`.
+Returns metadata for dimensions and metrics available in reporting methods. Used to explore the dimensions and metrics. In this method, a Google Analytics GA4 Property Identifier is specified in the request, and the metadata response includes Custom dimensions and metrics as well as Universal metadata. For example if a custom metric with parameter name `levels_unlocked` is registered to a property, the Metadata response will contain `customEvent:levels_unlocked`. Universal metadata are dimensions and metrics applicable to any property such as `country` and `totalUsers`.
 */
 await gapi.client.analyticsdata.properties.getMetadata({ name: "name",  });
 
