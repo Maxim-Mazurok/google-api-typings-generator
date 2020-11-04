99b944ec38d893ff5e567d0ffdc089deec7d821c
Maxim-Mazurok <Maxim-Mazurok@users.noreply.github.com>
2020-11-04 02:47:44 +0000

    Automated deployment: Wed Nov  4 02:47:44 UTC 2020 597afdc764d72094f32c7a1b21c40e1c96451573

--- gapi.client.fcm/index.d.ts
+++ gapi.client.fcm/index.d.ts
@@ -1,7 +1,6 @@
 /* Type definitions for non-npm package Firebase Cloud Messaging API v1 1.0 */
 // Project: https://firebase.google.com/docs/cloud-messaging
 // Definitions by: Maxim Mazurok <https://github.com/Maxim-Mazurok>
-//                 Google API Typings Generator <https://github.com/google-api-typings-generator>
 //                 Nick Amoscato <https://github.com/namoscato>
 //                 Declan Vong <https://github.com/declanvong>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
--- gapi.client.workflowexecutions/index.d.ts
+++ gapi.client.workflowexecutions/index.d.ts
@@ -1,7 +1,6 @@
 /* Type definitions for non-npm package Workflow Executions API v1beta 1.0 */
 // Project: https://cloud.google.com/workflows
 // Definitions by: Maxim Mazurok <https://github.com/Maxim-Mazurok>
-//                 Google API Typings Generator <https://github.com/google-api-typings-generator>
 //                 Nick Amoscato <https://github.com/namoscato>
 //                 Declan Vong <https://github.com/declanvong>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
