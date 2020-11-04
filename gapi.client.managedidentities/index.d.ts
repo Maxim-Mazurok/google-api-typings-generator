9f7a6e4f32cc40bb1e185479bc16e6506ae53e32
Maxim-Mazurok <Maxim-Mazurok@users.noreply.github.com>
2020-11-04 20:47:38 +0000

    Automated deployment: Wed Nov  4 20:47:37 UTC 2020 597afdc764d72094f32c7a1b21c40e1c96451573

--- gapi.client.managedidentities/index.d.ts
+++ gapi.client.managedidentities/index.d.ts
@@ -1,7 +1,6 @@
 /* Type definitions for non-npm package Managed Service for Microsoft Active Directory API v1 1.0 */
 // Project: https://cloud.google.com/managed-microsoft-ad/
 // Definitions by: Maxim Mazurok <https://github.com/Maxim-Mazurok>
-//                 Google API Typings Generator <https://github.com/google-api-typings-generator>
 //                 Nick Amoscato <https://github.com/namoscato>
 //                 Declan Vong <https://github.com/declanvong>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
