8c09fb3c9583b4b3bf3898a711564f005ac9b2d1
Maxim-Mazurok <Maxim-Mazurok@users.noreply.github.com>
2020-11-04 04:49:50 +0000

    Automated deployment: Wed Nov  4 04:49:50 UTC 2020 597afdc764d72094f32c7a1b21c40e1c96451573

--- gapi.client.games/index.d.ts
+++ gapi.client.games/index.d.ts
@@ -1,7 +1,6 @@
 /* Type definitions for non-npm package Google Play Game Services v1 1.0 */
 // Project: https://developers.google.com/games/
 // Definitions by: Maxim Mazurok <https://github.com/Maxim-Mazurok>
-//                 Google API Typings Generator <https://github.com/google-api-typings-generator>
 //                 Nick Amoscato <https://github.com/namoscato>
 //                 Declan Vong <https://github.com/declanvong>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
