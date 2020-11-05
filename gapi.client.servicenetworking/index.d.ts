e0ca252cbac736850cad2c222bf1dd2f89baf8ea
Maxim-Mazurok <Maxim-Mazurok@users.noreply.github.com>
2020-11-05 17:48:07 +0000

    Automated deployment: Thu Nov  5 17:48:07 UTC 2020 ae4f5d4e79158191b13b5c17a779f9c04dae6b77

--- gapi.client.servicenetworking/index.d.ts
+++ gapi.client.servicenetworking/index.d.ts
@@ -164,8 +164,8 @@ declare namespace gapi.client {
             issuer?: string;
             /**
              * URL of the provider's public key set to validate signature of the JWT. See [OpenID Discovery](https://openid.net/specs/openid-connect-discovery-1_0.html#ProviderMetadata). Optional
-             * if the key set document: - can be retrieved from [OpenID Discovery](https://openid.net/specs/openid-connect-discovery-1_0.html of the issuer. - can be inferred from the email domain
-             * of the issuer (e.g. a Google service account). Example: https://www.googleapis.com/oauth2/v1/certs
+             * if the key set document: - can be retrieved from [OpenID Discovery](https://openid.net/specs/openid-connect-discovery-1_0.html) of the issuer. - can be inferred from the email
+             * domain of the issuer (e.g. a Google service account). Example: https://www.googleapis.com/oauth2/v1/certs
              */
             jwksUri?: string;
             /**
@@ -1095,6 +1095,10 @@ declare namespace gapi.client {
             /** The source syntax. */
             syntax?: string;
         }
+        interface UpdateConsumerConfigRequest {
+            /** Required. The updated peering config. */
+            consumerConfig?: ConsumerConfig;
+        }
         // tslint:disable-next-line:no-empty-interface
         interface UpdateDnsRecordSetMetadata {
         }
@@ -1936,6 +1940,72 @@ declare namespace gapi.client {
             }): Request<ListPeeredDnsDomainsResponse>;
         }
         interface NetworksResource {
+            /** Service producers use this method to update the configuration of their connection including the import/export of custom routes and subnetwork routes with public IP. */
+            updateConsumerConfig(request: {
+                /** V1 error format. */
+                "$.xgafv"?: string;
+                /** OAuth access token. */
+                access_token?: string;
+                /** Data format for response. */
+                alt?: string;
+                /** JSONP */
+                callback?: string;
+                /** Selector specifying which fields to include in a partial response. */
+                fields?: string;
+                /** API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token. */
+                key?: string;
+                /** OAuth 2.0 token for the current user. */
+                oauth_token?: string;
+                /**
+                 * Required. Parent resource identifying the connection for which the consumer config is being updated in the format:
+                 * `services/{service}/projects/{project}/global/networks/{network}` {service} is the peering service that is managing connectivity for the service producer's organization. For
+                 * Google services that support this functionality, this value is `servicenetworking.googleapis.com`. {project} is the number of the project that contains the service consumer's
+                 * VPC network e.g. `12345`. {network} is the name of the service consumer's VPC network.
+                 */
+                parent: string;
+                /** Returns response with indentations and line breaks. */
+                prettyPrint?: boolean;
+                /** Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters. */
+                quotaUser?: string;
+                /** Upload protocol for media (e.g. "raw", "multipart"). */
+                upload_protocol?: string;
+                /** Legacy upload protocol for media (e.g. "media", "multipart"). */
+                uploadType?: string;
+                /** Request body */
+                resource: UpdateConsumerConfigRequest;
+            }): Request<Operation>;
+            updateConsumerConfig(request: {
+                /** V1 error format. */
+                "$.xgafv"?: string;
+                /** OAuth access token. */
+                access_token?: string;
+                /** Data format for response. */
+                alt?: string;
+                /** JSONP */
+                callback?: string;
+                /** Selector specifying which fields to include in a partial response. */
+                fields?: string;
+                /** API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token. */
+                key?: string;
+                /** OAuth 2.0 token for the current user. */
+                oauth_token?: string;
+                /**
+                 * Required. Parent resource identifying the connection for which the consumer config is being updated in the format:
+                 * `services/{service}/projects/{project}/global/networks/{network}` {service} is the peering service that is managing connectivity for the service producer's organization. For
+                 * Google services that support this functionality, this value is `servicenetworking.googleapis.com`. {project} is the number of the project that contains the service consumer's
+                 * VPC network e.g. `12345`. {network} is the name of the service consumer's VPC network.
+                 */
+                parent: string;
+                /** Returns response with indentations and line breaks. */
+                prettyPrint?: boolean;
+                /** Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters. */
+                quotaUser?: string;
+                /** Upload protocol for media (e.g. "raw", "multipart"). */
+                upload_protocol?: string;
+                /** Legacy upload protocol for media (e.g. "media", "multipart"). */
+                uploadType?: string;
+            },
+            body: UpdateConsumerConfigRequest): Request<Operation>;
             peeredDnsDomains: PeeredDnsDomainsResource;
         }
         interface GlobalResource {
