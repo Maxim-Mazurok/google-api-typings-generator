1c03c2ba616f4ebee3790db268a4207ec4a2070d
Maxim-Mazurok <Maxim-Mazurok@users.noreply.github.com>
2020-12-10 17:18:40 +0000

    Automated deployment: Thu Dec 10 17:18:39 UTC 2020 26e9d22fd9a9edca7b66ae4bbd2060992c915f70

--- gapi.client.iap/gapi.client.iap-tests.ts
+++ gapi.client.iap/gapi.client.iap-tests.ts
@@ -4,7 +4,7 @@
 * In case of any problems please post issue to https://github.com/Maxim-Mazurok/google-api-typings-generator
 **/
 
-// Revision: 20201106
+// Revision: 20201204
 
 gapi.load('client', () => {
     /** now we can use gapi.client */
@@ -55,7 +55,6 @@ gapi.load('client', () => {
             policy: {
                 bindings: [
                     {
-                        bindingId: "Test string",
                         condition: {
                             description: "Test string",
                             expression: "Test string",
