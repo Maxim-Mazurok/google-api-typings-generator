4802fbd44485e646f1dd6501156be10a5089df54
Maxim-Mazurok <Maxim-Mazurok@users.noreply.github.com>
2021-01-21 02:34:12 +0000

    Automated deployment: Thu Jan 21 02:34:12 UTC 2021 55f363e3ee0b9bd415242db89b326f882f322b06

--- gapi.client.vectortile/gapi.client.vectortile-tests.ts
+++ gapi.client.vectortile/gapi.client.vectortile-tests.ts
@@ -4,7 +4,7 @@
 * In case of any problems please post issue to https://github.com/Maxim-Mazurok/google-api-typings-generator
 **/
 
-// Revision: 20210118
+// Revision: 20210120
 
 gapi.load('client', () => {
     /** now we can use gapi.client */
