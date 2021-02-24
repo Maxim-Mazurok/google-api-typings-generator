254d2f6b738270a23e89baf7e52a8bf01e6668d7
Maxim-Mazurok <Maxim-Mazurok@users.noreply.github.com>
2021-02-24 11:07:37 +0000

    Automated deployment: Wed Feb 24 11:07:37 UTC 2021 738dc1b31f7fe7b6d3dfbea8bbecb7faeb701ffe

--- gapi.client.datacatalog/gapi.client.datacatalog-tests.ts
+++ gapi.client.datacatalog/gapi.client.datacatalog-tests.ts
@@ -4,7 +4,7 @@
 * In case of any problems please post issue to https://github.com/Maxim-Mazurok/google-api-typings-generator
 **/
 
-// Revision: 20210209
+// Revision: 20210220
 
 gapi.load('client', () => {
     /** now we can use gapi.client */
@@ -32,7 +32,7 @@ gapi.load('client', () => {
     async function run() {
         /**
          * Searches Data Catalog for multiple resources like entries, tags that match a query. This is a custom method (https://cloud.google.com/apis/design/custom_methods) and does not return the
-         * complete resource, only the resource identifier and high level fields. Clients can subsequentally call `Get` methods. Note that Data Catalog search queries do not guarantee full recall.
+         * complete resource, only the resource identifier and high level fields. Clients can subsequently call `Get` methods. Note that Data Catalog search queries do not guarantee full recall.
          * Query results that match your query may not be returned, even in subsequent result pages. Also note that results returned (and not returned) can vary across repeated search queries. See
          * [Data Catalog Search Syntax](https://cloud.google.com/data-catalog/docs/how-to/search-reference) for more information.
          */
