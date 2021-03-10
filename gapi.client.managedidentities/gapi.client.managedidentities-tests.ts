7e11cfbe04ec67e4380cb0e2d44d72406db4625d
Maxim-Mazurok <Maxim-Mazurok@users.noreply.github.com>
2021-03-10 13:57:55 +0000

    Automated deployment: Wed Mar 10 13:57:54 UTC 2021 8935248ad80e110117538d0e7bf215bba9982c97

--- gapi.client.managedidentities/gapi.client.managedidentities-tests.ts
+++ gapi.client.managedidentities/gapi.client.managedidentities-tests.ts
@@ -4,7 +4,7 @@
 * In case of any problems please post issue to https://github.com/Maxim-Mazurok/google-api-typings-generator
 **/
 
-// Revision: 20210218
+// Revision: 20210305
 
 gapi.load('client', () => {
     /** now we can use gapi.client */
