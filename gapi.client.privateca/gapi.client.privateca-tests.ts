15aea424f4c9ab5e03d9a70a498eb3c59339f46a
Maxim-Mazurok <Maxim-Mazurok@users.noreply.github.com>
2021-03-17 05:07:00 +0000

    Automated deployment: Wed Mar 17 05:06:59 UTC 2021 21d671ce095af37915a9e8f14a2b34d5dc14e7a7

--- gapi.client.privateca/gapi.client.privateca-tests.ts
+++ gapi.client.privateca/gapi.client.privateca-tests.ts
@@ -4,7 +4,7 @@
 * In case of any problems please post issue to https://github.com/Maxim-Mazurok/google-api-typings-generator
 **/
 
-// Revision: 20210218
+// Revision: 20210316
 
 gapi.load('client', () => {
     /** now we can use gapi.client */
@@ -15,7 +15,7 @@ gapi.load('client', () => {
         /** declare client_id registered in Google Developers Console */
         const client_id = '<<PUT YOUR CLIENT ID HERE>>';
         const scope = [
-            /** View and manage your data across Google Cloud Platform services */
+            /** See, edit, configure, and delete your Google Cloud Platform data */
             'https://www.googleapis.com/auth/cloud-platform',
         ];
         const immediate = false;
