652523069e4c81a0b63937d0c72a4171abd1d187
Maxim-Mazurok <Maxim-Mazurok@users.noreply.github.com>
2021-03-18 19:55:22 +0000

    Automated deployment: Thu Mar 18 19:55:21 UTC 2021 21d671ce095af37915a9e8f14a2b34d5dc14e7a7

--- gapi.client.datamigration/gapi.client.datamigration-tests.ts
+++ gapi.client.datamigration/gapi.client.datamigration-tests.ts
@@ -4,7 +4,7 @@
 * In case of any problems please post issue to https://github.com/Maxim-Mazurok/google-api-typings-generator
 **/
 
-// Revision: 20210307
+// Revision: 20210317
 
 gapi.load('client', () => {
     /** now we can use gapi.client */
@@ -15,7 +15,7 @@ gapi.load('client', () => {
         /** declare client_id registered in Google Developers Console */
         const client_id = '<<PUT YOUR CLIENT ID HERE>>';
         const scope = [
-            /** View and manage your data across Google Cloud Platform services */
+            /** See, edit, configure, and delete your Google Cloud Platform data */
             'https://www.googleapis.com/auth/cloud-platform',
         ];
         const immediate = false;
