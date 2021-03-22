fa31ecf97dbb5e4f13bcb2e0081eb21cd527fe6f
Maxim-Mazurok <Maxim-Mazurok@users.noreply.github.com>
2021-03-22 16:02:14 +0000

    Automated deployment: Mon Mar 22 16:02:14 UTC 2021 4e1783bdcd7149bf08746fda0c6b73a0189b2752

--- gapi.client.area120tables/gapi.client.area120tables-tests.ts
+++ gapi.client.area120tables/gapi.client.area120tables-tests.ts
@@ -4,7 +4,7 @@
 * In case of any problems please post issue to https://github.com/Maxim-Mazurok/google-api-typings-generator
 **/
 
-// Revision: 20210318
+// Revision: 20210319
 
 gapi.load('client', () => {
     /** now we can use gapi.client */
@@ -17,7 +17,7 @@ gapi.load('client', () => {
         const scope = [
             /** See, edit, create, and delete all of your Google Drive files */
             'https://www.googleapis.com/auth/drive',
-            /** View and manage Google Drive files and folders that you have opened or created with this app */
+            /** See, edit, create, and delete only the specific Google Drive files you use with this app */
             'https://www.googleapis.com/auth/drive.file',
             /** See and download all your Google Drive files */
             'https://www.googleapis.com/auth/drive.readonly',
