83b60fc77f513f7f4ec2ee22122da43fe209e5c7
Maxim-Mazurok <Maxim-Mazurok@users.noreply.github.com>
2022-02-04 09:57:34 +0000

    deploy: 17e1ce9cb4ee7a9a136ee9585318481a695162d5

--- gapi.client.baremetalsolution/gapi.client.baremetalsolution-tests.ts
+++ gapi.client.baremetalsolution/gapi.client.baremetalsolution-tests.ts
@@ -7,7 +7,7 @@
 
 gapi.load('client', () => {
     /** now we can use gapi.client */
-    gapi.client.load('baremetalsolution', 'v2', () => {
+    gapi.client.load('baremetalsolution', 'v1', () => {
         /** now we can use gapi.client.baremetalsolution */
 
         /** don't forget to authenticate your client before sending any request to resources: */
@@ -29,5 +29,38 @@ gapi.load('client', () => {
     });
 
     async function run() {
+        /**
+         * Starts asynchronous cancellation on a long-running operation. The server makes a best effort to cancel the operation, but success is not guaranteed. If the server doesn't support this
+         * method, it returns `google.rpc.Code.UNIMPLEMENTED`. Clients can use Operations.GetOperation or other methods to check whether the cancellation succeeded or whether the operation
+         * completed despite cancellation. On successful cancellation, the operation is not deleted; instead, it becomes an operation with an Operation.error value with a google.rpc.Status.code of
+         * 1, corresponding to `Code.CANCELLED`.
+         */
+        await gapi.client.baremetalsolution.operations.cancel({
+            name: "Test string",
+        }, {
+        });
+        /**
+         * Deletes a long-running operation. This method indicates that the client is no longer interested in the operation result. It does not cancel the operation. If the server doesn't support
+         * this method, it returns `google.rpc.Code.UNIMPLEMENTED`.
+         */
+        await gapi.client.baremetalsolution.operations.delete({
+            name: "Test string",
+        });
+        /** Gets the latest state of a long-running operation. Clients can use this method to poll the operation result at intervals as recommended by the API service. */
+        await gapi.client.baremetalsolution.operations.get({
+            name: "Test string",
+        });
+        /**
+         * Lists operations that match the specified filter in the request. If the server doesn't support this method, it returns `UNIMPLEMENTED`. NOTE: the `name` binding allows API services to
+         * override the binding to use different resource name schemes, such as `users/*‍/operations`. To override the binding, API services can add a binding such as
+         * `"/v1/{name=users/*}/operations"` to their service configuration. For backwards compatibility, the default name includes the operations collection id, however overriding users must
+         * ensure the name binding is the parent resource, without the operations collection id.
+         */
+        await gapi.client.baremetalsolution.operations.list({
+            filter: "Test string",
+            name: "Test string",
+            pageSize: 42,
+            pageToken: "Test string",
+        });
     }
 });
