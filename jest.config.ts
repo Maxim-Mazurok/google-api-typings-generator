581a6ff1412b6a72d9644bbdbbcc8915654f61c2
Maxim Mazurok <maxim@mazurok.com>
2022-09-26 17:41:20 +1000

    defaultsESM preset issue

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,15 @@
 import {JestConfigWithTsJest} from 'ts-jest';
+import {defaultsESM} from 'ts-jest/presets';
+
+const defaultsESMPatched = defaultsESM;
+defaultsESMPatched.transform!['^.+\\.tsx?$'] = ['ts-jest', {useESM: true}]; // TODO: remove this once https://github.com/kulshekhar/ts-jest/pull/2207 is released
+// defaultsESMPatched.extensionsToTreatAsEsm =
+//   defaultsESMPatched.extensionsToTreatAsEsm?.filter(x => x !== '.mts');
 
 const config: JestConfigWithTsJest = {
-  extensionsToTreatAsEsm: ['.ts'],
+  ...defaultsESMPatched,
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
-  transform: {
-    '^.+\\.ts$': ['ts-jest', {useESM: true}],
   },
   testRegex: '/test/.*(?<!\\.integration)\\.spec\\.ts$',
 };
