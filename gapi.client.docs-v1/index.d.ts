948e877e2c1e778d10a8d9def489117aa82e53a6
Maxim-Mazurok <Maxim-Mazurok@users.noreply.github.com>
2023-02-28 09:13:08 +0000

    deploy: 5bfc49ea586a7806c9a9d871a822510d53083245

--- gapi.client.docs-v1/index.d.ts
+++ gapi.client.docs-v1/index.d.ts
@@ -583,7 +583,7 @@ declare namespace gapi.client {
             objectSize?: Size;
             /**
              * The image URI. The image is fetched once at insertion time and a copy is stored for display inside the document. Images must be less than 50MB in size, cannot exceed 25 megapixels,
-             * and must be in one of PNG, JPEG, or GIF format. The provided URI can be at most 2 kB in length. The URI itself is saved with the image, and exposed via the
+             * and must be in one of PNG, JPEG, or GIF format. The provided URI must be publicly accessible and at most 2 kB in length. The URI itself is saved with the image, and exposed via the
              * ImageProperties.content_uri field.
              */
             uri?: string;
