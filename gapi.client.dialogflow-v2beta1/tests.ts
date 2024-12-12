f18d0d19e84958ff9204dd5f783788e91d62dea1
Maxim-Mazurok <Maxim-Mazurok@users.noreply.github.com>
2024-12-12 19:40:15 +0000

    deploy: 35c6dedcedcdc76bf4111ffb562f6c5d07466459

--- gapi.client.dialogflow-v2beta1/tests.ts
+++ gapi.client.dialogflow-v2beta1/tests.ts
@@ -8282,7 +8282,7 @@ gapi.load('client', async () => {
         importGcsCustomMetadata: true,
       }
     );
-    /** Starts asynchronous cancellation on a long-running operation. The server makes a best effort to cancel the operation, but success is not guaranteed. If the server doesn't support this method, it returns `google.rpc.Code.UNIMPLEMENTED`. Clients can use Operations.GetOperation or other methods to check whether the cancellation succeeded or whether the operation completed despite cancellation. On successful cancellation, the operation is not deleted; instead, it becomes an operation with an Operation.error value with a google.rpc.Status.code of `1`, corresponding to `Code.CANCELLED`. */
+    /** Starts asynchronous cancellation on a long-running operation. The server makes a best effort to cancel the operation, but success is not guaranteed. If the server doesn't support this method, it returns `google.rpc.Code.UNIMPLEMENTED`. Clients can use Operations.GetOperation or other methods to check whether the cancellation succeeded or whether the operation completed despite cancellation. On successful cancellation, the operation is not deleted; instead, it becomes an operation with an Operation.error value with a google.rpc.Status.code of 1, corresponding to `Code.CANCELLED`. */
     await gapi.client.dialogflow.projects.locations.operations.cancel({
       name: 'Test string',
     });
@@ -8731,7 +8731,7 @@ gapi.load('client', async () => {
         sessionId: 'Test string',
       }
     );
-    /** Starts asynchronous cancellation on a long-running operation. The server makes a best effort to cancel the operation, but success is not guaranteed. If the server doesn't support this method, it returns `google.rpc.Code.UNIMPLEMENTED`. Clients can use Operations.GetOperation or other methods to check whether the cancellation succeeded or whether the operation completed despite cancellation. On successful cancellation, the operation is not deleted; instead, it becomes an operation with an Operation.error value with a google.rpc.Status.code of `1`, corresponding to `Code.CANCELLED`. */
+    /** Starts asynchronous cancellation on a long-running operation. The server makes a best effort to cancel the operation, but success is not guaranteed. If the server doesn't support this method, it returns `google.rpc.Code.UNIMPLEMENTED`. Clients can use Operations.GetOperation or other methods to check whether the cancellation succeeded or whether the operation completed despite cancellation. On successful cancellation, the operation is not deleted; instead, it becomes an operation with an Operation.error value with a google.rpc.Status.code of 1, corresponding to `Code.CANCELLED`. */
     await gapi.client.dialogflow.projects.operations.cancel({
       name: 'Test string',
     });
