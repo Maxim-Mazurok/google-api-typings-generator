82050ee188cef9eda638a67f6f08036dead40b55
Maxim Mazurok <maxim@mazurok.com>
2025-03-24 12:07:56 +1100

    fix observability:v1 documentationLink

--- src/constants.ts
+++ src/constants.ts
@@ -6,6 +6,8 @@ export const fallbackDocumentationLinks: {
   >;
 } = {
   'tasks:v1': 'https://developers.google.com/tasks',
+  'observability:v1':
+    'https://cloud.google.com/distributed-cloud/hosted/docs/latest/gdch/apis/service/observability/observability-api-overview',
 };
 
 export const zeroWidthJoinerCharacter = String.fromCharCode(8205);
