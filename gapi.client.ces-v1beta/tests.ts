d132e2ff2a3d806e49598aca1b51ad76747ce168
Maxim-Mazurok <Maxim-Mazurok@users.noreply.github.com>
2026-03-06 16:08:05 +0000

    deploy: 8bb696fe9671d20dcdd590b6136ce26f78a40477

--- gapi.client.ces-v1beta/tests.ts
+++ gapi.client.ces-v1beta/tests.ts
@@ -294,6 +294,9 @@ gapi.load('client', async () => {
         args: {
           A: 42,
         },
+        context: {
+          A: 42,
+        },
         tool: 'Test string',
         toolsetTool: {
           toolId: 'Test string',
